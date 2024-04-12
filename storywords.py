09905c362586669cc9cd88e68ea943774c452b3b
kali15-jpg <kali15@students.kennesaw.edu>
2024-04-12 11:43:57 -0400

    made storywords and genres variable

---
+++ storywords.py
@@ -0,0 +1,7 @@
+# import random module
+import random
+# list of genres for user to pick
+genres = ['fantasy', 'history', 'action', 'classic']
+# print whichever genre the user selected
+print(random.choise(genres))
+
