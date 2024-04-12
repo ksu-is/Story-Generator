dc142e59422193c6c55bba8459928b367fe054a5
kali15-jpg <kali15@students.kennesaw.edu>
2024-04-12 12:02:30 -0400

    did plot variable

--- storywords.py
+++ storywords.py
@@ -6,3 +6,4 @@ genres = ['fantasy', 'history', 'action', 'classic']
 print(random.choise(genres))
 # define lists to start story
 intro = ['Once upon a time', 'Long ago', 'Wandering through the darkness', 'The sun shone and birds were chirping merrily']
+plot = ['there was a lost kingdom', 'there was a grand civilization', 'I crept around looking for light', 'I was reading my book and sitting against a tree']
