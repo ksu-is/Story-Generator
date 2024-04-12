e3b8b7df6e1fffb9a0e340c70c73db2cddaf4e57
kali15-jpg <kali15@students.kennesaw.edu>
2024-04-12 11:57:23 -0400

    finished intro variable

--- storywords.py
+++ storywords.py
@@ -5,4 +5,4 @@ genres = ['fantasy', 'history', 'action', 'classic']
 # print whichever genre the user selected
 print(random.choise(genres))
 # define lists to start story
-intro = ['Once upon a time', 'Long ago',]
+intro = ['Once upon a time', 'Long ago', 'Wandering through the darkness', 'The sun shone and birds were chirping merrily']
