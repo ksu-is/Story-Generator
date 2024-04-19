5c0d0b2547869958fca994bd1019340a5cf555ad
kali15-jpg <kali15@students.kennesaw.edu>
2024-04-19 09:12:59 -0400

    print statement works

--- storywords.py
+++ storywords.py
@@ -3,7 +3,11 @@ import random
 # list of genres for user to pick
 genres = ['fantasy', 'history', 'action', 'classic']
 # print whichever genre the user selected
-print(random.choise(genres))
+print(random.choice(genres))
 # define lists to start story
-intro = ['Once upon a time', 'Long ago', 'Wandering through the darkness', 'The sun shone and birds were chirping merrily']
-plot = ['there was a lost kingdom', 'there was a grand civilization', 'I crept around looking for light', 'I was reading my book and sitting against a tree']
+intro = ['Once upon a time ', 'Long ago ', 'Wandering through the darkness ', 'The sun shone and birds were chirping merrily ']
+plot = ['there was a lost kingdom ', 'there was a grand civilization ', 'I crept around looking for light ', 'I was reading my book and sitting against a tree ']
+climax = ['']
+end = ['']
+# select an item from lists to print story
+print(random.choice(intro)+random.choice(plot))
