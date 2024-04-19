c9ef78f3a9360c0a1421e4862c3c3e8defa27114
kali15-jpg <kali15@students.kennesaw.edu>
2024-04-19 09:25:06 -0400

    added questions for user

--- storywords.py
+++ storywords.py
@@ -2,12 +2,16 @@
 import random
 # list of genres for user to pick
 genres = ['fantasy', 'history', 'action', 'classic']
+print("Which genre would you like?" + genres)
 # print whichever genre the user selected
 print(random.choice(genres))
 # define lists to start story
+characters = ['one', 'two', 'three']
+print("How many characters would you like?" + characters)
 intro = ['Once upon a time ', 'Long ago ', 'Wandering through the darkness ', 'The sun shone and birds were chirping merrily ']
-plot = ['there was a lost kingdom ', 'there was a grand civilization ', 'I crept around looking for light ', 'I was reading my book and sitting against a tree ']
-climax = ['']
+print("What setting would you like?" + setting)
+setting = ['there was a lost kingdom ', 'there was a grand civilization ', 'I crept around looking for light ', 'I was reading my book and sitting against a tree ']
+climax = ['who needed a hero to restore it']
 end = ['']
 # select an item from lists to print story
 print(random.choice(intro)+random.choice(plot))
